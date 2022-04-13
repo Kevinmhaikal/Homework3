3d37adbe1c6efa6e995773f37e44bf29d606ad98
kevinmuhammadhaikal <kevinmuhammadhaikal@gmail.com>
2022-04-13 10:13:53 +0700

    Tugas 13

--- kevin/src/App.js
+++ kevin/src/App.js
@@ -1,34 +1,23 @@
-import { useSelector } from "react-redux";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from "react-router-dom";
-import CreatePlaylist from "./pages/CreatePlaylist";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const token = useSelector((state) => state.token.value);
-
-  // here is the songs view
   return (
-    <div className="p-5 bg-neutral-900 h-screen space-y-5 overflow-auto">
-      {/* if token is empty, redirect to login*/}
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            {!token ? <Login /> : <Redirect to="/create-playlist" />}
-          </Route>
-          <Route path="/create-playlist">
-            <CreatePlaylist />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- kevin/src/pages/NotFound/index.js
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="flex justify-center items-center h-full text-white text-3xl">
-      <p>404 NOT FOUND</p>
-    </div>
-  );
-};
-
-export default NotFound;
